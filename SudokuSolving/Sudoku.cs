57a9cf0e86e224df67bf607c3f534d9ec4a6ab17
Robert Danielsson <robert.danielsson@yh.nackademin.se>
2018-09-18 21:30:22 +0200

    Försökte skapa upp en metod som kollar efter möjliga siffror att sätta in på tomma platser. Saknas lite logik men just nu skapar den en array som är lika stor som Board arrayen och fyller den med 1or eller 0or beroende på om det finns en siffra på plats redan (0 räknas som tom plats).

--- SudokuSolving/Sudoku.cs
+++ SudokuSolving/Sudoku.cs
@@ -28,13 +28,53 @@ namespace SudokuSolver
             {
                 for (int col = 0; col < 9; col++)
                 {
-                    if(board[row, col] == 0)
+                    if (board[row, col] == 0)
                     {
                         return false;
                     }
                 }
             }
             return true;
+        }
+
+        public void FindPossibleEntries(int[,] board, int row, int col)
+        {
+            int[,] possibilitiesArray = new int[9, 9];
+
+            for (int y = 0; y < 9; y++) //Fyller vår possibilitesArray med 0or som grund.
+            {
+                for (int x = 0; x < 9; x++)
+                {
+                    possibilitiesArray[y, x] = 0;
+                }
+            }
+            for (int x = 0; x < 9; x++) //Kollar vågrätt om innehållet är en 0a, annars sätts indexet till en 1a.
+            {
+                if (board[row, x] != 0)
+                {
+                    possibilitiesArray[row, x] = 1;
+                }
+            }
+            for (int y = 0; y < 9; y++) //Kollar horisontellt om innehållet är en 0a, annars sätts indexet till en 1a.
+            {
+                if (board[y, col] != 0)
+                {
+                    possibilitiesArray[y, col] = 1;
+                }
+            }
+
+            int k = 0;
+            int l = 0;
+
+            if (row >= 0 && row <= 2) k = 0;
+            else if (row >= 3 && row <= 5) k = 3;
+            else k = 6;
+
+            if (col >= 0 && col <= 2) l = 0;
+            else if (col >= 3 && col <= 5) l = 3;
+            else l = 6;
+
+
         }
 
         public int[,] FillBoard(string boardString)
