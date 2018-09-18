04950aac6930199bd6c570b4e4c52111d291e27f
Robert Danielsson <robert.danielsson@yh.nackademin.se>
2018-09-18 20:12:10 +0200

    Skapade upp en grund för att ladda upp i CMS18, hade tidigare ett gemensamt github-projekt under min profil

--- SudokuSolving/Program.cs
+++ SudokuSolving/Program.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SudokuSolving
+namespace SudokuSolver
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Sudoku game = new Sudoku("003020600900305001001806400" +
+                                     "008102900700000008006708200" +
+                                     "002609500800203009005010300");
+
+            game.Solve();
         }
     }
 }
---
+++ SudokuSolving/Sudoku.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    class Sudoku
+    {
+        public string BoardString { get; set; }
+        public int[,] Board = new int[9, 9];
+
+        public Sudoku(string boardString)
+        {
+            BoardString = boardString;
+        }
+
+        public void Solve()
+        {
+            Board = FillBoard(BoardString);
+            PrintSudokuBoard(Board);
+        }
+
+        public bool IsboardFull(int[,] board)
+        {
+            for (int row = 0; row < 9; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+                    if(board[row, col] == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public int[,] FillBoard(string boardString)
+        {
+            int[,] board = new int[9, 9];
+
+            if (boardString.Length == 81)
+            {
+                char[] boardCharArray = boardString.ToCharArray();
+
+                int col = 0;
+                int row = 0;
+
+                foreach (char character in boardCharArray)
+                {
+                    int currentNumber = int.Parse(character.ToString());
+                    board[col, row] = currentNumber;
+
+                    row++;
+                    if (row == 9)
+                    {
+                        col++;
+                        row = 0;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Incorrect amount of numbers in boardstring!");
+            }
+            return board;
+        }
+
+        public void PrintSudokuBoard(int[,] board)
+        {
+            Console.Write("+-----------------------------------+\n");
+
+            for (int col = 0; col < 9; col++)
+            {
+                Console.Write("|");
+
+                for (int row = 0; row < 9; row++)
+                {
+                    Console.Write(" " + board[col, row] + " |");
+
+                    if (row == 8)
+                    {
+                        Console.Write("\n+-----------------------------------+");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
+
